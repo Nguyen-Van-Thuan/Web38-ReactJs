6cc98865a11f909f4960a68c02505d75acbdca2b
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2024-05-08 19:53:35 +0700

    update code

--- project-ecommerce/src/App.jsx
+++ project-ecommerce/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/admin/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route />
-        
+        <Route path='/dashboad/home' element={<Home/>}/>
+
       </Routes>
     </BrowserRouter>
   )
