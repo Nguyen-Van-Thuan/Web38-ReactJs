31f1028c7efbb652f4d1d316ffa5f7af162d416d
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2024-05-08 19:30:56 +0700

    Update b9

---
+++ project-ecommerce/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// import css dashboad
+import "./assets/css/bootstrap.min.css"
+import "./assets/css/bootstrap-icons.css"
+import "./assets/css/apexcharts.css"
+import "./assets/css/main.css"
+
+// import js dasbhoad
+import "./assets/js/jquery.min.js"
+import "./assets/js/bootstrap.bundle.min.js"
+import "./assets/js/apexcharts.min.js"
+import "./assets/js/custom.js"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
