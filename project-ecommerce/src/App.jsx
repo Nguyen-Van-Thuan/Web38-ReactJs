7a7fd3aae2cb5225a35b77a641bb35036a89aaa3
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2024-05-08 21:21:11 +0700

    update b9

--- project-ecommerce/src/App.jsx
+++ project-ecommerce/src/App.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/admin/Home'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/admin/Home";
+import Layout from "./pages/admin/Layout";
+import Wallet from "./pages/admin/Wallet";
+import NotFound from "./pages/admin/NotFound";
+import Profile from "./pages/admin/Profile";
+import Setting from "./pages/admin/Setting";
+import Help from "./pages/admin/Help";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/dashboad" element={<Layout />}>
 
-        <Route path='/dashboad/home' element={<Home/>}/>
+          <Route index element={<Home />} />
+          <Route path="/dashboad/wallet" element={<Wallet/>}/>
+          <Route path="/dashboad/profile" element={<Profile/>} />
+          <Route path="/dashboad/setting" element={<Setting/>}/>
+          <Route path="/dashboad/help" element={<Help/>}/>
 
+          <Route path="*" element={<NotFound/>}/>
+
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- project-ecommerce/src/pages/admin/Home.jsx
+++ project-ecommerce/src/pages/admin/Home.jsx
@@ -1,9 +1,154 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Sidebar from "../../components/admin/Sidebar";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  useEffect(() => {
+    // Chart 1
+    var options_1 = {
+      series: [13, 43, 22],
+      chart: {
+        width: 380,
+        type: "pie",
+      },
+      labels: ["Balance", "Expense", "Credit Loan"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: "bottom",
+            },
+          },
+        },
+      ],
+    };
 
-export default Home
+    var chart_1 = new ApexCharts(document.querySelector("#pie-chart"), options_1);
+    chart_1.render();
+
+    // Chart 2
+    var options = {
+      series: [
+        {
+          name: "Income",
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+        },
+        {
+          name: "Expense",
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+        },
+        {
+          name: "Transfer",
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+        },
+      ],
+      chart: {
+        type: "bar",
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%",
+          endingShape: "rounded",
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"],
+      },
+      xaxis: {
+        categories: [
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+        ],
+      },
+      yaxis: {
+        title: {
+          text: "$ (thousands)",
+        },
+      },
+      fill: {
+        opacity: 1,
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return "$ " + val + " thousands";
+          },
+        },
+      },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+
+    // Khi component truoc khi unmount
+    return () =>{
+      chart_1.destroy();
+      chart.destroy();
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          {/* SIDEBAR MENU  */}
+          <Sidebar />
+
+          {/* MAIN CONTENT */}
+          <main className="main-wrapper col-md-9 ms-sm-auto py-4 col-lg-9 px-md-4 border-start">
+            <div className="title-group mb-3">
+              <h1 className="h2 mb-0">Overview</h1>
+              <small className="text-muted">Hello Thomas, welcome back!</small>
+            </div>
+            <div className="row my-4">
+              <div className="col-lg-12 col-12">
+                <div className="custom-block bg-white">
+                  <h5 className="mb-4">History</h5>
+                  <div id="pie-chart" />
+                </div>
+              </div>
+              <div className="col-lg-12 col-12">
+                <div className="custom-block bg-white">
+                  <div id="chart" />
+                </div>
+              </div>
+            </div>
+            <footer className="site-footer">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12 col-12">
+                    <p className="copyright-text">
+                      Copyright © Convert React - Design:{" "}
+                      <a rel="sponsored" href="/" target="_blank">
+                        Thuan Nguyen
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </footer>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
